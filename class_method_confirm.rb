4b5aa6fd9060eb5cf87ae27f0d06548485bcd8ee
huro3h <huro3h@gmail.com>
2018-06-18 18:41:45 +0900

    Confirm class method

---
+++ class_method_confirm.rb
@@ -0,0 +1,17 @@
+class A
+  def self.method1
+    p Module.nesting
+  end
+end
+# A.method1
+# => [A]
+
+class B
+  class << self
+    def method2
+      p Module.nesting
+    end
+  end
+end
+# B.method2
+# => [#<Class:B>, B]
